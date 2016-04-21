7b06a908a2a7dd3fab3d39189b6b6e3d98ffbccc
jgeigerm <jmg2967@rit.edu>
2016-04-21 10:22:29 -0400

    added replaceHookedMethod example as comment

--- yak/app/src/main/java/de/havefuninsi/Yak/YakLog.java
+++ yak/app/src/main/java/de/havefuninsi/Yak/YakLog.java
@@ -21,7 +21,14 @@ public class YakLog implements IXposedHookLoadPackage {
             }
         });
         //disable certificate pinning
-        findAndHookMethod("qn", lpparam.classLoader, "verify", String.class, SSLSession.class, XC_MethodReplacement.returnConstant(true));
         findAndHookMethod("nC", lpparam.classLoader, "a", String.class, List.class, XC_MethodReplacement.DO_NOTHING);
+        /*findAndHookMethod("nC", lpparam.classLoader, "a", String.class, List.class, new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log((String)param.args[0]);
+                return null;
+            }
+        });*/
+
     }
 }
